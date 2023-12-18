eb2cb8dc2695d0ffd0a56aa80473b6c1423e31e5
habibashawkat <habiba2109942@miuegypt.edu.eg>
2023-12-18 16:16:15 +0200

    Update buttons.dart

--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -1,29 +1,49 @@
+import 'package:career_coach/screens/login_page.dart';
 import 'package:flutter/material.dart';
+
 
 class MyButton extends StatelessWidget {
   final String text;
   const MyButton({Key? key, required this.text}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(40),
-      ),
-      padding: EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            text,
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(width: 10),
-          Icon(
-            Icons.arrow_forward,
-            color: Colors.white,
-          ),
-        ],
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    // Calculate font size based on screen width
+    double fontSize = screenWidth * 0.05; // Adjust this multiplier as needed
+
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the login page when the button is pressed
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => loginPage()),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                color: Color(0xff0f4f6c),
+                fontSize: fontSize,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Icon(
+              Icons.arrow_forward,
+              color: Color(0xff0f4f6c),
+            ),
+          ],
+        ),
       ),
     );
   }
