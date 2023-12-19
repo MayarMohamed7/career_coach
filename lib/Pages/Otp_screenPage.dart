3dbe5ba23d1c23c540186422aed649571113d137
ShahdMady <shahd2010797@miuegypt.edu.eg>
2023-12-20 00:26:04 +0200

    shahd

---
+++ lib/Pages/Otp_screenPage.dart
@@ -0,0 +1,104 @@
+import 'package:career_coach/Pages/Reservation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class OTPScreen extends StatefulWidget {
+  final String sessionId;
+
+  const OTPScreen({Key? key, required this.sessionId}) : super(key: key);
+
+  @override
+  _OTPScreenState createState() => _OTPScreenState();
+}
+
+class _OTPScreenState extends State<OTPScreen> {
+  final TextEditingController otpController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff0f4f6c),
+        title: Text('Enter OTP'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: otpController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'OTP'),
+              validator: (value) {
+                if (value == null || value.isEmpty || value.length != 6) {
+                  return 'Please enter a valid 6-digit OTP';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                // Verify OTP logic
+                if (otpController.text.length == 6) {
+                  // Save reservation details to Firebase
+                  String userId = FirebaseAuth.instance.currentUser!
+                      .uid; // Replace with the actual user ID
+
+                  _saveReservation(userId, widget.sessionId);
+
+                  // Change status to "unavailable" in sessions table
+                  await updateSessionStatus(widget.sessionId);
+
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: Text('Verification'),
+                      content: Text('Verified and reservation done.'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('Okay'),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (ctx) => ReservationListPage()),
+                            ); // Close the dialog
+                            // Optionally navigate away or perform other actions
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xff0f4f6c),
+                foregroundColor: Colors.white,
+              ),
+              child: Text('Verify OTP'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _saveReservation(String userId, String sessionId) async {
+    FirebaseFirestore.instance.collection('reservations').add({
+      'userId': userId,
+      'sessionId': sessionId,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+  }
+
+  Future<void> updateSessionStatus(String sessionId) async {
+    await FirebaseFirestore.instance
+        .collection('sessions')
+        .doc(sessionId)
+        .update({
+      'status': 'unavailable',
+    });
+  }
+}
--- lib/Pages/payment_page.dart
+++ lib/Pages/payment_page.dart
@@ -1,56 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PaymentPage extends StatefulWidget {
-  const PaymentPage({super.key});
+  final String sessionId;
 
+  const PaymentPage({Key? key, required this.sessionId}) : super(key: key);
   @override
-  // ignore: library_private_types_in_public_api
   _PaymentPageState createState() => _PaymentPageState();
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
   TextEditingController cardNumberController = TextEditingController();
   TextEditingController cardHolderNameController = TextEditingController();
   TextEditingController expiryMonthController = TextEditingController();
   TextEditingController expiryYearController = TextEditingController();
   TextEditingController cvvController = TextEditingController();
 
-  void _showOTPDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Enter OTP'),
-          content: TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(labelText: 'OTP'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Cancel button
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Pay'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xff0f4f6c),
-        title: const Text('Payment Form'),
+        title: Text('Payment Form'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -61,7 +34,7 @@ class _PaymentPageState extends State<PaymentPage> {
             children: [
               TextFormField(
                 controller: cardHolderNameController,
-                decoration: const InputDecoration(labelText: 'Cardholder Name'),
+                decoration: InputDecoration(labelText: 'Cardholder Name'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter the cardholder name';
@@ -69,44 +42,58 @@ class _PaymentPageState extends State<PaymentPage> {
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 16),
               TextFormField(
                 controller: cardNumberController,
                 keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Card Number'),
+                decoration: InputDecoration(labelText: 'Card Number'),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(16),
+                ],
                 validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid card number';
+                  if (value == null || value.isEmpty || value.length < 16) {
+                    return 'Please enter a valid 16-digit card number';
                   }
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 16),
               Row(
                 children: [
                   Expanded(
                     child: TextFormField(
                       controller: expiryMonthController,
                       keyboardType: TextInputType.number,
-                      decoration:
-                          const InputDecoration(labelText: 'Expiry Month'),
+                      decoration: InputDecoration(labelText: 'Expiry Month'),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(2),
+                      ],
                       validator: (value) {
-                        if (value == null || value.isEmpty) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length != 2) {
                           return 'Please enter a valid expiry month';
                         }
                         return null;
                       },
                     ),
                   ),
-                  const SizedBox(width: 16),
+                  SizedBox(width: 16),
                   Expanded(
                     child: TextFormField(
                       controller: expiryYearController,
                       keyboardType: TextInputType.number,
-                      decoration:
-                          const InputDecoration(labelText: 'Expiry Year'),
+                      decoration: InputDecoration(labelText: 'Expiry Year'),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(4),
+                      ],
                       validator: (value) {
-                        if (value == null || value.isEmpty) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length != 4) {
                           return 'Please enter a valid expiry year';
                         }
                         return null;
@@ -115,29 +102,39 @@ class _PaymentPageState extends State<PaymentPage> {
                   ),
                 ],
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 16),
               TextFormField(
                 controller: cvvController,
                 keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'CVV'),
+                decoration: InputDecoration(labelText: 'CVV'),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(3),
+                ],
                 validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a valid CVV';
+                  if (value == null || value.isEmpty || value.length != 3) {
+                    return 'Please enter a valid 3-digit CVV';
                   }
                   return null;
                 },
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 16),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   if (_formKey.currentState!.validate()) {
-                    _showOTPDialog();
+                    // Navigate to the OTP screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              OTPScreen(sessionId: widget.sessionId)),
+                    );
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xff0f4f6c),
+                  primary: Color(0xff0f4f6c),
                 ),
-                child: const Text('Next'),
+                child: Text('Next'),
               ),
             ],
           ),
