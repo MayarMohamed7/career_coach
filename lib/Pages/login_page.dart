92a375aa9129d799a71560b3c88fe42d7c3283ce
MayarMohamed7 <mayar2012932@miuegypt.edu.eg>
2024-01-09 20:05:13 +0200

    login coach home

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:career_coach/Pages/coachHome.dart';
 import 'package:career_coach/Pages/passwordReset.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -24,8 +25,8 @@ class _loginPageState extends State<loginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
- AuthService auth_methods = new AuthService();
- bool _isLoading = false;
+  AuthService auth_methods = new AuthService();
+  bool _isLoading = false;
 
   @override
   void dispose() {
@@ -38,9 +39,9 @@ class _loginPageState extends State<loginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffffffff),
-     appBar: AppBar(
-          backgroundColor: Color(0xff0f4f6c),
-          title: Row(
+      appBar: AppBar(
+        backgroundColor: Color(0xff0f4f6c),
+        title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Image.asset(
@@ -50,7 +51,7 @@ class _loginPageState extends State<loginPage> {
             SizedBox(width: 8),
           ],
         ),
-        ),
+      ),
       body: Container(
         padding: const EdgeInsets.all(16),
         margin: const EdgeInsets.all(16),
@@ -59,20 +60,19 @@ class _loginPageState extends State<loginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-             children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                       Text(
-                'Please SignIn Here!',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                ),
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Please SignIn Here!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ],
               ),
-                  
-                  ],
-                ),
               SizedBox(height: 10),
               TextField(
                 controller: _emailController,
@@ -116,37 +116,38 @@ class _loginPageState extends State<loginPage> {
               ),
               SizedBox(height: 20),
               GestureDetector(
-               child:Text(
-                'Forgot Password?',
-                style:  TextStyle(
-                     color: Color(0xff0f4f6c),
-                        decoration: TextDecoration.underline,
-                        fontWeight: FontWeight.bold,
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Color(0xff0f4f6c),
+                    decoration: TextDecoration.underline,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-               ),
                 onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => passwordResetpage()));
-                      },
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => passwordResetpage()));
+                },
               ),
               SizedBox(height: 20),
               ElevatedButton(
-                  onPressed: () async {
-                    String? signInResult =
-                        await AuthService().signInWithEmailAndPassword(
-                      email: _emailController.text,
-                      password: _passwordController.text,
-                    );
-                    setState(() {
-                      _isLoading = true;
-                    });
+                onPressed: () async {
+                  String? signInResult =
+                      await AuthService().signInWithEmailAndPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                  );
+                  setState(() {
+                    _isLoading = true;
+                  });
 
-                    if (signInResult == null) {
-                      // Check if the user exists in the 'users' collection
-                      bool isLoggedIn = await auth_methods.checkAuthenticationStatus(); 
-                      if (isLoggedIn){
+                  if (signInResult == null) {
+                    // Check if the user exists in the 'users' collection
+                    bool isLoggedIn =
+                        await auth_methods.checkAuthenticationStatus();
+                    if (isLoggedIn) {
                       bool isUser = await AuthService()
                           .checkIfUserExists(_emailController.text);
                       bool isCoach = await AuthService()
@@ -162,52 +163,49 @@ class _loginPageState extends State<loginPage> {
                         // Redirect to the new session page if the user exists in the 'coaches' collection
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => NewSession()),
+                          MaterialPageRoute(builder: (context) => CoachHome()),
                         );
                       }
                       setState(() {
                         _isLoading = false;
                       });
-                      }
-                       else {
-                        // Handle scenario where the user is neither in 'users' nor 'coaches'
-                        print('User not found in either table');
-                      }
                     } else {
-                      // Sign-in failed, show an error message or handle the error
-                      print("Sign-in failed: $signInResult");
-                      
-
-                      // Show an error message using ScaffoldMessenger's snackbar with duration of 3 seconds
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text("Sign-in failed: $signInResult"),
-                          duration: Duration(seconds: 5),
-                          backgroundColor: Colors.red,
-                        ),
-                      );
+                      // Handle scenario where the user is neither in 'users' nor 'coaches'
+                      print('User not found in either table');
                     }
-                       setState(() {
-                        _isLoading = false;
-                      });
-                  },
-                  child: _isLoading
-                      ? CircularProgressIndicator() // Show progress indicator when loading
-                      : Text('Login'), // Show "Sign Up" text otherwise
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff0f4f6c),
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32.0),
-                    ),
+                  } else {
+                    // Sign-in failed, show an error message or handle the error
+                    print("Sign-in failed: $signInResult");
+
+                    // Show an error message using ScaffoldMessenger's snackbar with duration of 3 seconds
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Sign-in failed: $signInResult"),
+                        duration: Duration(seconds: 5),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                  setState(() {
+                    _isLoading = false;
+                  });
+                },
+                child: _isLoading
+                    ? CircularProgressIndicator() // Show progress indicator when loading
+                    : Text('Login'), // Show "Sign Up" text otherwise
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff0f4f6c),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32.0),
                   ),
                 ),
-                SizedBox(height: 10),
-              ],
-            ),
+              ),
+              SizedBox(height: 10),
+            ],
           ),
         ),
-      );
-    
+      ),
+    );
   }
 }
